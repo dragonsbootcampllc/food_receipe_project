8c743d6a74e3bf930e67b198ed4a3a71ed8a6ba4
Salaheldin Mohamed <75949002+salah-mo@users.noreply.github.com>
2023-05-09 00:23:45 +0300

    edit home

--- src/js/header_navigator_links.mjs
+++ src/js/header_navigator_links.mjs
@@ -1,28 +1,26 @@
-let nav_object={
-  "navItems": [
+let nav_object = {
+  navItems: [
     {
-      "label": "Home",
-      "url": "/src/html"
+      label: "Home",
+      url: "/src/html/index.html",
     },
     {
-      "label": "About",
-      "url": "/src/html/about.html"
+      label: "About",
+      url: "/src/html/about.html",
     },
     {
-      "label": "Contact",
-      "url": "/src/html/contact.html"
+      label: "Contact",
+      url: "/src/html/contact.html",
     },
-  ]
+  ],
 };
-let nav_ul=document.getElementById("header_navigator_ul");
+let nav_ul = document.getElementById("header_navigator_ul");
 
-for(const element of nav_object.navItems){
-  let li=document.createElement("li");
-  let a=document.createElement("a");
-  a.href=element.url;
-  a.innerHTML=element.label;
+for (const element of nav_object.navItems) {
+  let li = document.createElement("li");
+  let a = document.createElement("a");
+  a.href = element.url;
+  a.innerHTML = element.label;
   li.appendChild(a);
   nav_ul.appendChild(li);
-}  
-
-
+}
